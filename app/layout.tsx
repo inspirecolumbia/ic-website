17d078035eb1830c592238ce3d2f310e2321015a
Maximus Fernandez <maximusfernandez98@gmail.com>
2026-07-31 11:13:25 -0400

    init supabase db with shadcn components

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import "./globals.css";
 import ScrollToTop from "../components/ScrollToTop";
 import Footer from "../components/Footer";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://inspirecolumbia.org"),
@@ -55,7 +59,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", geist.variable)}>
       <body>
         <ScrollToTop />
         {children}
